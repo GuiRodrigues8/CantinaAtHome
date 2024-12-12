8334aa49285ef2d5bef8bfd224485bac74520f9a
GuiRodrigues8 <gfr04@hotmail.com>
2024-12-12 20:34:00 +0000

    Fixed error in AvisoLegal

--- src/pages/avisoLegal/AvisoLegal.tsx
+++ src/pages/avisoLegal/AvisoLegal.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         <div id = "avisoLegal" className="avisoLegal">
             <div className="avisoLegalText">
                 <h1>Aviso Legal</h1>
-                <p>Os conteúdos presentes neste perfil foram realizados por alunos do 3º ano da Licenciatura de Engenharia Informática da Faculdade de Ciências e Tecnologias da Universidade de Coimbra (FCTUC), no âmbito de uma disciplina denominada Processos de Gestão e Inovação. Deste modo, a FCTUC não se responsabiliza pelo seu conteúdo.</p>
+                <p>Os conteúdos presentes neste website foram realizados por alunos do 3º ano da Licenciatura de Engenharia Informática da Faculdade de Ciências e Tecnologias da Universidade de Coimbra (FCTUC), no âmbito de uma disciplina denominada Processos de Gestão e Inovação. Deste modo, a FCTUC não se responsabiliza pelo seu conteúdo.</p>
             </div>
         </div>
     )
