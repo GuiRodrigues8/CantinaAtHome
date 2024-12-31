4b25f1674e11e5c776bd138a1a5c828968c516cd
GuiRodrigues8 <gfr04@hotmail.com>
2024-12-31 16:32:13 +0000

    Added demo video and fixed mobile and tablet visualization!

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -2,10 +2,11 @@ import logo from '../assets/logo.png';
 import about from '../assets/about.png';
 import demo from '../assets/demo.png';
 import instagram from '../assets/instagram.png';
-
+import demoVideo from '../assets/demo.mp4';
 export default {
     logo,
     about,
     demo,
-    instagram
+    instagram,
+    demoVideo
 };
--- src/pages/demo/Demo.tsx
+++ src/pages/demo/Demo.tsx
@@ -1,16 +1,42 @@
-import "../demo/Demo.css"
-import images from "../../constants/images"
+import "../demo/Demo.css";
+import images from "../../constants/images";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import { useState, useRef } from "react";
 
 export default function Demo() {
+    const [isPlaying, setIsPlaying] = useState(false); // Estado para controlar o vídeo
+    const videoRef = useRef<HTMLVideoElement>(null); // Define o tipo correto para o vídeo
+
+    const handleVideoToggle = () => {
+        if (videoRef.current) { // Verifica se videoRef.current não é null
+            if (isPlaying) {
+                videoRef.current.pause(); // Pausa o vídeo
+            } else {
+                videoRef.current.play(); // Inicia o vídeo
+            }
+            setIsPlaying(!isPlaying); // Atualiza o estado
+        }
+    };
+
     return (
-        <div id = "demo" className = "demo">
-            <div className="demo_img">
-                <img src = {images.demo} alt = "demo" ></img>
-            </div>
-            <div className="demo_text">
-                <p>Ainda em desenvolvimento!!</p>
-                <p>Esperem por novidades!!! </p>
+        <div id="demo" className="demo">
+            <div className="video_container">
+                <video
+                    ref={videoRef}
+                    muted
+                    loop
+                    id="myVideo"
+                    className="video_player"
+                >
+                    <source src={images.demoVideo} type="video/mp4" />
+                </video>
+                <button
+                    onClick={handleVideoToggle}
+                    className="video_toggle_button"
+                >
+                    {isPlaying ? <BsPauseFill size={24} /> : <BsFillPlayFill size={24} />}
+                </button>
             </div>
         </div>
-    )
+    );
 }
